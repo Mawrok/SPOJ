5850d27b503d8fd7c5a5904cc6f8f40894793169
Mawrok <60617904+Mawrok@users.noreply.github.com>
2020-02-03 18:31:45 +0100

    Create  PRIME_T - Liczby Pierwsze.py

---
+++ PRIME_T - Liczby Pierwsze.py
@@ -0,0 +1,7 @@
+# https://pl.spoj.com/problems/PRIME_T/
+from math import sqrt
+
+tests = int(input())
+for i in range(tests):
+    num = int(input())
+    print(("NIE", "TAK")[num >= 2 and all(num % i for i in range(3, 1 + int(sqrt(num)), 2))])
