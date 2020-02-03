2a54ef2353e6844b0d6ef6ee8d7b025f252751f0
Mawrok <60617904+Mawrok@users.noreply.github.com>
2020-02-03 17:53:12 +0100

    Create  PRZEDSZK - Przedszkolanka.py

---
+++ PRZEDSZK - Przedszkolanka.py
@@ -0,0 +1,7 @@
+# https://pl.spoj.com/problems/PRZEDSZK/
+from fractions import gcd
+ 
+tests = int(input())
+for i in range(tests):
+    [a, b] = map(int, input().split())
+    print(a * b // gcd(a, b))
