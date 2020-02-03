2189381451a6101389ab33858ba32e630d7d0b18
Mawrok <60617904+Mawrok@users.noreply.github.com>
2020-02-03 22:04:33 +0100

    Create JZLICZ - Zliczacz liter.py

---
+++ JZLICZ - Zliczacz liter.py
@@ -0,0 +1,11 @@
+# https://pl.spoj.com/problems/JZLICZ/
+from collections import Counter
+
+counts = Counter()
+tests = int(input())
+
+for i in range(tests):
+    counts.update(Counter(input().replace(' ', '')))
+
+for let, count in sorted(counts.items(), key=lambda keyval: (not keyval[0].islower(), keyval[0])):
+    print(let, count)
