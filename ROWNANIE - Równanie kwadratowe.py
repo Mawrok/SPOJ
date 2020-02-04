b683716bba9dd86015b1d6a54c9168e75eb54ec0
Mawrok <60617904+Mawrok@users.noreply.github.com>
2020-02-04 20:17:00 +0100

    Update ROWNANIE - Równanie kwadratowe.py

--- ROWNANIE - Równanie kwadratowe.py
+++ ROWNANIE - Równanie kwadratowe.py
@@ -1,8 +1,7 @@
 # https://pl.spoj.com/problems/ROWNANIE/
-while True:
-    try:
-        [a, b, c] = map(float, input().split())
-        discriminant = b * b - 4 * a * c
-        print(1 + int(discriminant > 0) - int(discriminant < 0))
-    except EOFError:
-        break
+from sys import stdin
+
+for line in stdin:
+    [a, b, c] = map(float, line.split())
+    discriminant = b * b - 4 * a * c
+    print(1 + int(discriminant > 0) - int(discriminant < 0))
