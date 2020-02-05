c5cb0380d7ba43b72db8c4a86acf32998991e35e
Mawrok <60617904+Mawrok@users.noreply.github.com>
2020-02-05 18:32:28 +0100

    Update JPESEL - Pesel.py

--- JPESEL - Pesel.py
+++ JPESEL - Pesel.py
@@ -1,8 +1,7 @@
-# https://pl.spoj.com/problems/JPESEL/
 from operator import mul
 weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1]
 tests = int(input())
 for i in range(tests):
-    pesel = list(map(int, input()))
+    pesel = map(int, input())
     is_valid = sum(map(mul, pesel, weights)) % 10 == 0
     print(("N", "D")[is_valid])
