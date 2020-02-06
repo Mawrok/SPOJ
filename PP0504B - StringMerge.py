f870adcbfc67dc33536c16fdf10743efd72534a6
Mawrok <60617904+Mawrok@users.noreply.github.com>
2020-02-06 19:28:09 +0100

    Update PP0504B - StringMerge.py

--- PP0504B - StringMerge.py
+++ PP0504B - StringMerge.py
@@ -1,7 +1,7 @@
 # https://pl.spoj.com/problems/PP0504B/
 tests = int(input())
 for i in range(tests):
-    [first, second] = input().split()
+    first, second = input().split()
     for j in range(min(len(first), len(second))):
         print(first[j], second[j], sep="", end="")
     print()
