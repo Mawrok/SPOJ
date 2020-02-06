127c4689c3c6a7cf77ad3bc9196206a07f82bb6f
Mawrok <60617904+Mawrok@users.noreply.github.com>
2020-02-06 19:11:32 +0100

    Update PA05_POT - Czy umiesz potęgować.py

--- PA05_POT - Czy umiesz potęgować.py
+++ PA05_POT - Czy umiesz potęgować.py
@@ -1,5 +1,5 @@
 # https://pl.spoj.com/problems/PA05_POT/
 tests = int(input())
 for i in range(tests):
-    [base, exp] = map(int, input().split())
+    base, exp = map(int, input().split())
     print((base % 10) ** ((exp - 1) % 4 + 1) % 10)
